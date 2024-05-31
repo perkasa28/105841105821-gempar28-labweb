93fe655196df05276adf0cf1b0ad59ac9d4801ff
perkasa28 <105841105821@student.unismuh.ac.id>
2024-05-31 10:43:00 +0800

    update

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 const App = () => {
   return (
+    <View style={{flex:1}}>
     <View style={{flex:1, flexDirection:'row'}}>
       <View style={{
         flex:1,
@@ -10,9 +11,18 @@ const App = () => {
         }}></View>
         <View style={{
         flex:1,
-        backgroundColor:'blue'
+        backgroundColor:'blue',
         }}></View>
     </View>
+    <View style={{
+        flex:1,
+        backgroundColor:'green',
+        }}></View>
+        <View style={{
+        flex:1,
+        backgroundColor:'brown',
+        }}></View>
+  </View>
   )
 }
 
